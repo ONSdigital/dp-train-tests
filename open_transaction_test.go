a51011c271b65efabb9274a068f031412c6aaa5f
Dave Llewellyn <439837@gmail.com>
2020-12-08 18:07:01 +0000

    simplified tests to rely on train instance already running

--- open_transaction_test.go
+++ open_transaction_test.go
@@ -12,18 +12,34 @@ func Test_beingTransaction(t *testing.T) {
 	Convey("Given an instance of the Train is running", t, func() {
 		theTrain := train.NewClient()
 
-		Convey("When Begin is invoked", func() {
+		Convey("When a begin new transaction request is sent", func() {
 			transaction, err := theTrain.Begin()
 			So(err, ShouldBeNil)
+			So(transaction, ShouldNotBeNil)
+
+			defer transaction.CleanUp()
 
 			Convey("Then a new transaction is created", func() {
-				t := train.GetInstance()
+				So(transaction, ShouldCreateTransactionDir)
+				So(transaction, ShouldCreateBackupDir)
+				So(transaction, ShouldCreateContentDir)
+				So(transaction, ShouldCreateTransactionJSON)
+			})
+		})
+	})
+}
 
-				So(transaction, ShouldNotBeNil)
-				So(t, ShouldCreateTransactionDir, transaction)
-				So(t, ShouldCreateBackupDir, transaction)
-				So(t, ShouldCreateContentDir, transaction)
-				So(t, ShouldCreateTransactionJSON, transaction)
+func Test_healthCheck(t *testing.T) {
+	Convey("Given a healthy instance of the Train is running", t, func() {
+		theTrain := train.NewClient()
+
+		Convey("When a health check request is sent", func() {
+			status, err := theTrain.HealthCheck()
+
+			Convey("Then a healthy status response is returned", func() {
+				So(err, ShouldBeNil)
+				So(status, ShouldNotBeNil)
+				So(status.Message, ShouldEqual, "OK")
 			})
 		})
 	})
